cfb800c47d4f6e5f12680d1b46b1d5316cfc6ee0
Orditus <jessy.tachi@edu.ece.fr>
2023-11-25 11:34:22 +0100

    Commit Bryan

--- Bryan.c
+++ Bryan.c
@@ -15,3 +15,23 @@ struct Graphe {
     int* degreEntrant;
 };
 
+struct Noeud* creerNoeud(int dest) {
+    struct Noeud* nouveauNoeud = (struct Noeud*)malloc(sizeof(struct Noeud));
+    nouveauNoeud->dest = dest;
+    nouveauNoeud->suivant = NULL;
+    return nouveauNoeud;
+}
+
+struct Graphe* creerGraphe(int V) {
+    struct Graphe* graphe = (struct Graphe*)malloc(sizeof(struct Graphe));
+    graphe->V = V;
+    graphe->listesAdj = (struct Noeud**)malloc(V * sizeof(struct Noeud*));
+    graphe->degreEntrant = (int*)malloc(V * sizeof(int));
+
+    for (int i = 0; i < V; i++) {
+        graphe->listesAdj[i] = NULL;
+        graphe->degreEntrant[i] = 0;
+    }
+
+    return graphe;
+}

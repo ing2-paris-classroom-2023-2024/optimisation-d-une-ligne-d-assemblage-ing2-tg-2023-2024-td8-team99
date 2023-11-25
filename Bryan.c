f73c64a518a29f44d98729369298d7e41d877879
Orditus <jessy.tachi@edu.ece.fr>
2023-11-25 11:29:10 +0100

    Commit Bryan

--- Bryan.c
+++ Bryan.c
@@ -1,4 +1,23 @@
 //
 // Created by Romaric on 20/11/2023.
 //
-test
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Noeud {
+    int dest;
+    struct Noeud* suivant;
+};
+
+struct Graphe {
+    int V;
+    struct Noeud** listesAdj;
+    int* degreEntrant;
+};
+
+struct Noeud* creerNoeud(int dest) {
+    struct Noeud* nouveauNoeud = (struct Noeud*)malloc(sizeof(struct Noeud));
+    nouveauNoeud->dest = dest;
+    nouveauNoeud->suivant = NULL;
+    return nouveauNoeud;
+}

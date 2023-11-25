cd24ab5274bfe1577c5a1cfc0d0ccbc473567915
Thibaut Larricart <thibaut.larricart@gmail.com>
2023-11-25 17:43:08 +0100

    structures thibaut

---
+++ cmake-build-debug/partieThibaut.h
@@ -0,0 +1,34 @@
+//
+// Created by thiba on 25/11/2023.
+//
+
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_TD8_TEAM99_PARTIETHIBAUT_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_TD8_TEAM99_PARTIETHIBAUT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure d'un arc
+typedef struct Arc
+    {
+    int numSommet; // numero de sommet d'un arc adjacent au sommet initial.
+    struct t_arc* arc_suivant;
+    int sommetInit;
+    int marquer;
+    }t_arc;
+
+typedef struct Sommet
+    {
+    t_arc* arc;
+    int couleur;
+    }*t_sommet;
+
+typedef struct Graphe
+    {
+    int taille;
+    int orientation;
+    int ordre;
+    t_sommet* t_sommet;
+    }t_graphe;
+
+#endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_TD8_TEAM99_PARTIETHIBAUT_H
--- partieThibaut.c
+++ partieThibaut.c
@@ -2,3 +2,16 @@
 // Created by thiba on 20/11/2023.
 //
 
+#include "partieThibaut.h"
+// Affichage des successeurs de chaque sommet
+void afficher_successeurs(t_sommet* sommet, int num)
+{
+    printf(" sommet %d :\n",num); //Affichage d un sommet
+    t_arc* arc=sommet[num]->arc;
+    while(arc!=NULL) //Permet de parcourir
+    {
+        printf("/%d/ ",arc->numSommet); // Affichage du successeur du sommet
+        printf("distance: %d\n", arc->valeur); //Affichage du poids
+        arc=arc->arc_suivant;
+    }
+}

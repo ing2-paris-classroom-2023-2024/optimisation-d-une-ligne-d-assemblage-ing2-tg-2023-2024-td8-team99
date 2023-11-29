79cfe4b70dfb5a8f163c7e1547905961f49a896a
NicoDuzan <nicolas.duzan@edu.ece.fr>
2023-11-29 17:30:45 +0100

    ajout du void et du début du main avec l'ouverture de fichier et la détermination du nombre des stations et de contraintes

--- exclusion.c
+++ exclusion.c
@@ -1,4 +1,17 @@
-//
-// Created by duni9 on 28/11/2023.
-//
-test
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE_MAX 100
+void welshPowell(int **matriceAdjacence, int nbStations, int *stationsExistantes);
+
+int main() {
+    //ouverture du fichier
+    FILE *fichier = fopen("exclusion.txt", "r");
+
+    if (fichier == NULL) {
+        perror("erreur lors de l'ouverture du fichier");
+        return EXIT_FAILURE;
+    }
+
+    //Determination du nombre de stations et de contraintes
+}

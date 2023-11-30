abea5afe36f601826aafd94a2954adf58548c5c6
NicoDuzan <nicolas.duzan@edu.ece.fr>
2023-11-30 13:56:26 +0100

    Ajout de la boucle pour la détermination du nombre de stations et allocation de la matrice d'adjacence

--- exclusion.c
+++ exclusion.c
@@ -14,4 +14,21 @@ int main() {
     }
 
     //Determination du nombre de stations et de contraintes
+    int nbStations = 0;
+    int nbContraintes = 0;
+    int station1 , station2;
+
+    while (fscanf(fichier, "%d %d", &station1, &station2)== 2) {
+        nbContraintes++;
+        if (station1 > nbStations) nbStations = station1;
+        if (station2 > nbStations) nbStations = station2;
+    }
+
+    fclose(fichier);
+
+    //Allocation de la matrice d'adjacence
+    int**matriceAdjacence = (int**) malloc((nbStations + 1) * sizeof(int *));
+    for (int i = 0; i<= nbStations;i++ ) {
+        matriceAdjacence[i] = (int*) calloc((nbStations + 1), sizeof (int));
+    }
 }
